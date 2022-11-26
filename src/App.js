fa4676fbc68318dbf805e604f6b8825ffc8e489f
diveshthakre19 <diveshthakre07@.com>
2022-11-26 12:15:34 +0530

    ading section1

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from './Components/Navbar'
 import Hero from './Components/Hero'
 import Counter from './Components/Counter'
+import Section1 from './Components/Section1'
 const App = () => {
   return (
     <div>
         <Navbar />
         <Hero />
         <Counter />
+        <Section1 />
     </div>
   )
 }
