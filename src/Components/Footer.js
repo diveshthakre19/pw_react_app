bafdc11a3e57e0fe35310e105958a13f9769f076
diveshthakre19 <diveshthakre07@gmail.com>
2022-11-30 23:45:52 +0530

    created footer  section

---
+++ src/Components/Footer.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="footer">
+        <div>
+          <img src="./images/down_app.png" alt="" />
+        </div>
+
+        <div className="socials">
+          <h3 className=" text-white tertary-heading">
+            follow us:
+            <img src="./images/fb.png" alt="" />
+            <img src="./images/utube.png" alt="" />
+            <img src="./images/inta.png" alt="" />
+            <img src="./images/in.svg" alt="" />
+          </h3>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
